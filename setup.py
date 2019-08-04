7fb11daf53ff82bf74f17209132301372911ad1b
Gilles Lenfant <gilles.lenfant@gmail.com>
2019-08-04 20:09:23 +0200

    Ignore sandbox and double extras_require removed

--- setup.py
+++ setup.py
@@ -9,8 +9,4 @@ PythonFileSystem extension for Azure Datalake Store
 from setuptools import setup
 
 dev_require = []
-setup(
-      test_suite='tests.all_tests',
-      extras_require={
-          'dev': dev_require
-      })
+setup()
