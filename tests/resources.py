035bd045c909fcf35bdf26d2dcd5fc62f5dd2c53
Gilles Lenfant <gilles.lenfant@gmail.com>
2019-08-06 23:07:26 +0200

    Starting tests

--- tests/resources.py
+++ tests/resources.py
@@ -4,15 +4,34 @@ import pathlib
 
 import dotenv
 
+from fs.subfs import ClosingSubFS
+import fs.path
+
+from fs.datalake import DatalakeFS
+
 tests_directory = pathlib.Path(__file__).resolve().parent
+
+remote_test_folder = "/billy/test_datalakefs"
 
 # Loading test environment vars
 _dotenv_path = dotenv.find_dotenv()
 dotenv.load_dotenv(_dotenv_path)
 
 tenant_id = os.getenv("DL_TENANT_ID")
+store_name = os.getenv("DL_STORE_NAME")
 client_id = os.getenv("DL_CLIENT_ID")
 client_secret = os.getenv("DL_CLIENT_SECRET")
 username = os.getenv("DL_USERNAME")
 password = os.getenv("DL_PASSWORD")
-store_name = os.getenv("DL_STORE_NAME")
+
+
+def make_fs_username():
+    filesystem = DatalakeFS(tenant_id, store_name, username=username, password=password)
+    filesystem.makedirs(remote_test_folder, recreate=True)
+    return filesystem.opendir(remote_test_folder, factory=ClosingSubFS)
+
+
+def make_fs_client_id():
+    filesystem = DatalakeFS(tenant_id, store_name, client_id=client_id, client_secret=client_secret)
+    filesystem.makedirs(remote_test_folder, recreate=True)
+    return filesystem.opendir(remote_test_folder, factory=ClosingSubFS)
---
+++ tests/test_resources.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Unit tests of fs.adls"""
+import os
+import unittest
+
+from tests.resources import make_fs_username, make_fs_client_id
+
+
+###
+# Test layers
+###
+
+def setUpModule():
+    # Setups for all tests of this module
+    return
+
+
+def tearDownModule():
+    # Cleanups for all tests of this module
+    return
+
+
+class ResourceTest(unittest.TestCase):
+    """Testing test resources"""
+
+    def test_environment_vars(self):
+        """The required environment vars are provided
+        """
+        self.assertIn("DL_TENANT_ID", os.environ)
+        self.assertIn("DL_STORE_NAME", os.environ)
+        self.assertIn("DL_CLIENT_ID", os.environ)
+        self.assertIn("DL_CLIENT_SECRET", os.environ)
+        self.assertIn("DL_USERNAME", os.environ)
+        self.assertIn("DL_PASSWORD", os.environ)
+        return
+
+    def test_make_fs_username(self):
+        make_fs_username()
+
+    def test_make_fs_client_id(self):
+        make_fs_client_id()
