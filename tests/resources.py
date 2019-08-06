9f6e28426ac54a64bf8ee080e854a2c35b2a3a01
Gilles Lenfant <gilles.lenfant-ext@energisme.com>
2019-08-06 18:40:51 +0200

    Base tests resources

--- tests/resources.py
+++ tests/resources.py
@@ -1,9 +1,18 @@
 """Misc fixtures and helpers for all tests"""
-
-# FIXME: Provide anything that suits your package tests
-# (custom TestCase classes, common fixtures, ...)
-import functools
 import os
+import pathlib
 
-tests_directory = os.path.dirname(os.path.abspath(__file__))
-tests_abs_path = functools.partial(os.path.join, tests_directory)
+import dotenv
+
+tests_directory = pathlib.Path(__file__).resolve().parent
+
+# Loading test environment vars
+_dotenv_path = dotenv.find_dotenv()
+dotenv.load_dotenv(_dotenv_path)
+
+tenant_id = os.getenv("DL_TENANT_ID")
+client_id = os.getenv("DL_CLIENT_ID")
+client_secret = os.getenv("DL_CLIENT_SECRET")
+username = os.getenv("DL_USERNAME")
+password = os.getenv("DL_PASSWORD")
+store_name = os.getenv("DL_STORE_NAME")
